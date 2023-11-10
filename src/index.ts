ff80f5f309e0403978bd544826e8bc1cb1f94526
inaniwaudon <w.yuto0916@gmail.com>
2023-11-10 16:00:39 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import { D1QB } from "workers-qb";
 import { Bindings } from "./bindings";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", (c) => c.text("Hello World!!!"));
+app.use(
+  "/api/*",
+  cors({
+    origin: ["http://localhost:3000", "https://resta-frontend.pages.dev"],
+  })
+);
+
+app.get("/api/formatdatas", (c) => {
+  // TODO
+  return c.text("TODO");
+});
+
+app.get("/api/formatdatas/:id", (c) => {
+  // TODO
+  return c.text("TODO");
+});
+
+const postFormatDatasSchema = z.object({
+  userId: z.string(),
+  title: z.string(),
+  images: z.array(z.string()),
+  description: z.string(),
+  tags: z.array(z.string()),
+  formatBlocks: z.array(z.unknown()),
+});
+
+app.post("/api/formatdatas", zValidator("json", postFormatDatasSchema), (c) => {
+  // TODO
+  return c.text("TODO");
+});
 
 export default app;
